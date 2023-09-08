c25362de7f4babde9e06788d88fc5be4f9a16bdc
App Generator <51070104+app-generator@users.noreply.github.com>
2023-09-08 19:31:39 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="copyright">
-      &copy; <a href="https://ternicos-construct.ro/">Ternicos Construct</a>, toate drepturile rezervate - Creat de <a href="https://websitemarket.ro/">WebsiteMarket</a>
+      &copy; <a href="https://ternicos-construct.ro/">Ternicos Construct</a>, Creat de <a href="https://websitemarket.ro/">WebsiteMarket</a>
     </div>
   );
 }
